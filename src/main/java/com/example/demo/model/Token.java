0916e581142e52b97444094d890bc7d14a8090af
mic <miclimule@gmail.com>
2022-12-14 23:30:17 +0300

    .

--- src/main/java/com/example/demo/model/Token.java
+++ src/main/java/com/example/demo/model/Token.java
@@ -2,13 +2,6 @@ package com.example.demo.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Token {
